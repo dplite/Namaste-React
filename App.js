20571e31ac082cdf3849e78ec4df6dbbac4c80cf
Divyanshu Pandey <pandeydivyanshu257@gmail.com>
2024-02-16 20:41:27 +0530

    JSX and React.create Element

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
+
+import React from "react"
+import ReactDOM from "react-dom/client"
 const h1 = React.createElement("h1", {id:"heading"}, "Hello World from React");
+
+
+
+let h1fromJSX=<h1 id="heading">Hello</h1>;  //JSX.. this is transpiled to React.create element by babel
+console.log(h1,h1fromJSX)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h1);
